66b29db6e09a1c0dc7b7135420c6110e60794aea
Glauber8289 <Petersonglauber@hotmail.com>
2023-07-02 23:03:04 -0300

    Seller and Entities class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,9 +11,12 @@ public class Program {
 		// TODO Auto-generated method stub
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
 		
-
+		Seller seller = new Seller(21, "bob", "bob@gmail", new Date(), 3000.00, obj);
+		
+		System.out.println(seller);
+		
+ 
 	}
 
 }
