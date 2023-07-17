b8ca4cbbf553f4de5dadfa2592b7161ec3bffc98
Glauber Peterson <107453279+Glauber8289@users.noreply.github.com>
2023-07-16 21:31:20 -0300

    Update Program.java

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,7 +23,7 @@ public class Program {
 		
 		System.out.println("\n=== Test 2 : seller findbyDepartment ====");
 		Department department = new Department(2,null);
-		List <Seller> list = sellerDao.findByDepartment(departments);
+		List <Seller> list = sellerDao.findByDepartment(department);
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
